d682a559532ec1474343534485bfccb34d81afa4
Danie83 <danut_fili2017@yahoo.com>
2022-10-31 19:50:33 +0200

    tests for user controller

--- project/backend/src/__tests__/user.controller.spec.ts
+++ project/backend/src/__tests__/user.controller.spec.ts
@@ -1,4 +1,5 @@
 import {UserController} from "../controller/user.controller";
+import {MOCK_USER} from "../const/const";
 
 const mockedEnd = jest.fn(() => Promise.resolve());
 const mockedResponse = {
@@ -7,10 +8,32 @@ const mockedResponse = {
 
 describe('User controller tests', function () {
 
+    test('user credential confirm - working', async () => {
+        const mockReq = jest.fn();
+        mockReq(MOCK_USER);
+        const result = UserController.verifyCredentials(mockReq as any, mockedResponse as any, null as any);
+        await expect(mockReq).toHaveBeenCalledTimes(1);
+        await expect(mockReq).toHaveBeenCalledWith(MOCK_USER);
+        result.then(async function(res){
+            await expect(res).toBe(true);
+        })
+    });
+
+    test('user credential confirm - fail', async () => {
+        const mockReq = jest.fn();
+        mockReq(MOCK_USER);
+        const result = UserController.verifyCredentials(mockReq as any, mockedResponse as any, null as any);
+        await expect(mockReq).toHaveBeenCalledTimes(1);
+        await expect(mockReq).toHaveBeenCalledWith(MOCK_USER);
+        result.then(async function(res){
+            await expect(res).toBe(false);
+        })
+    });
+
     test('asd', async () => {
         const result = UserController.getCoordinatorFiles(null as any, mockedResponse as any, null as any);
 
-        expect(mockedEnd).toHaveBeenCalledWith('Hello');
+        await expect(mockedEnd).toHaveBeenCalledWith('Hello');
     });
-
+    
 });
--- project/backend/src/controller/user.controller.ts
+++ project/backend/src/controller/user.controller.ts
@@ -5,4 +5,8 @@ export class UserController {
     static async getCoordinatorFiles(req: Request<any>, res: Response, next: NextFunction) {
         res.end();
     }
+
+    static async verifyCredentials(req: Request<any>, res: Response, next: NextFunction) {
+        return true;
+    }
 }
