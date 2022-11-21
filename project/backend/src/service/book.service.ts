2fb73ed6c720bec58f730bf77095599c7e0dd7ce
iceeye7gabi <iceeye7@yahoo.com>
2022-11-22 00:51:49 +0200

    added mop for services and update aop for controllers

--- project/backend/src/service/book.service.ts
+++ project/backend/src/service/book.service.ts
@@ -1,37 +1,51 @@
 import {Author, Book} from "../interface/interfaces";
 import {AuthorModel, BookModel} from "../database/models";
+import {Mop} from "../mop/mop";
 
 export class BookService {
     static async getBook(id: number): Promise<Book>{
-        return (await BookModel.findOne({
+        Mop.startCall("The monitor for getBook method from BookService is called with: " + id);
+        const result = (await BookModel.findOne({
             where: {
                 id: id,
             }
         }))?.toJSON() as Book;
+        Mop.endCall(result);
+        return result;
     }
     static async addBook(book: Book): Promise<void> {
+        Mop.startCall("The monitor for addBook method from BookService is called with: " + book);
         delete book.id;
 
         await BookModel.create({
             ...book
         });
+
+        Mop.endCall(true);
     }
 
     static async editBook(book: Book): Promise<void> {
+        Mop.startCall("The monitor for editBook method from BookService is called with: " + book);
         await BookModel.create({
             ...book
         });
+
+        Mop.endCall(true);
     }
 
     static async deleteBook(book: Book): Promise<void> {
+        Mop.startCall("The monitor for deleteBook method from BookService is called with: " + book);
         await BookModel.destroy({
             where: {
                 id: book.id,
             }
         });
+
+        Mop.endCall(true);
     }
 
     static async addAuthor(book: Book, author: Author): Promise<void> {
+        Mop.startCall("The monitor for addAuthor method from BookService for book and author is called with: " + {book, author});
         const bookModel = await BookModel.findOne({
             where: {
                 id: book.id,
@@ -43,12 +57,15 @@ export class BookService {
                 id: author.id,
             }
         });
+
+        Mop.endCall({bookModel, authorModel});
 
         // @ts-ignore
         bookModel.addAuthorModel(authorModel);
     }
 
     static async removeAuthor(book: Book, author: Author): Promise<void> {
+        Mop.startCall("The monitor for addAuthor method from BookService for book and author is called with: " + {book, author});
         const bookModel = await BookModel.findOne({
             where: {
                 id: book.id,
@@ -60,6 +77,8 @@ export class BookService {
                 id: author.id,
             }
         });
+
+        Mop.endCall({bookModel, authorModel});
 
         // @ts-ignore
         bookModel.destroyAuthorModel(authorModel);
--- project/backend/src/service/user.service.ts
+++ project/backend/src/service/user.service.ts
@@ -3,19 +3,24 @@ import {BookModel, PreferenceModel, UserModel} from "../database/models";
 import {ResponseError} from "../middleware/middleware";
 import {ResponseMessage, StatusCode} from "../const/const";
 import {JwtService} from "./jwt.service";
+import {Mop} from "../mop/mop";
 
 export class UserService {
 
     static async getUser(id: number): Promise<User> {
-        return (await UserModel.findOne({
+        Mop.startCall("The monitor for getUser method from UserService is called with: " + id);
+        const result = (await UserModel.findOne({
             where: {
                 id: id,
             },
             include: BookModel,
         }))?.toJSON() as User;
+        Mop.endCall(result);
+        return result;
     }
 
     static async loginUser(user: User) {
+        Mop.startCall("The monitor for loginUser method from UserService is called with: " + user);
         if (user.username == null || user.password == null) {
             throw new ResponseError(ResponseMessage.INVALID_CREDENTIALS, StatusCode.BAD_REQUEST);
         }
@@ -26,6 +31,8 @@ export class UserService {
             }
         });
 
+        Mop.endCall(userModel);
+
         if (userModel == null) {
             throw new ResponseError(ResponseMessage.USER_NOT_FOUND, StatusCode.NOT_FOUND);
         }
@@ -34,6 +41,7 @@ export class UserService {
     }
 
     static async signupUser(user: User) {
+        Mop.startCall("The monitor for signupUser method from UserService is called with: " + user);
         if (user.username == null || user.email == null || user.password == null) {
             throw new ResponseError(ResponseMessage.COMPLETE_ALL_FIELDS, StatusCode.BAD_REQUEST);
         }
@@ -46,32 +54,44 @@ export class UserService {
 
         await UserModel.create(newUser);
 
+        Mop.endCall(newUser);
+
         return JwtService.generateAccessTokenForStudent(newUser as User);
     }
 
     static async addUser(user: User): Promise<void> {
+        Mop.startCall("The monitor for addUser method from UserService is called with: " + user);
         delete user.id;
 
         await UserModel.create({
             ...user
         });
+
+        Mop.endCall(true);
     }
 
     static async editUser(user: User): Promise<void> {
+        Mop.startCall("The monitor for editUser method from UserService is called with: " + user);
         await UserModel.create({
             ...user
         });
+
+        Mop.endCall(true);
     }
 
     static async deleteUser(user: User): Promise<void> {
+        Mop.startCall("The monitor for deleteUser method from UserService is called with: " + user);
         await UserModel.destroy({
             where: {
                 id: user.id,
             }
         });
+
+        Mop.endCall(true);
     }
 
     static async addPreference(user: User, preference: Preference): Promise<void> {
+        Mop.startCall("The monitor for addPreference method from UserService for user and preference is called with: " + {user, preference});
         const userModel = await UserModel.findOne({
             where: {
                 id: user.id,
@@ -84,12 +104,15 @@ export class UserService {
                 name: preference.title,
             }
         });
+
+        Mop.endCall({userModel, preferenceModel});
 
         // @ts-ignore
         userModel.addPreferenceModel(preferenceModel);
     }
 
     static async removePreference(user: User, preference: Preference): Promise<void> {
+        Mop.startCall("The monitor for addPreference method from UserService for user and preference is called with: " + {user, preference});
         const userModel = await UserModel.findOne({
             where: {
                 id: user.id,
@@ -102,6 +125,8 @@ export class UserService {
                 name: preference.title,
             }
         });
+
+        Mop.endCall({userModel, preferenceModel});
 
         // @ts-ignore
         userModel.destroyPreferenceModel(preferenceModel);
