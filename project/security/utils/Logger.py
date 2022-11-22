b040a5b5d936ff93ac0cdc4c515f27be39b3be52
reloadedd <reloadedd@protonmail.com>
2022-11-23 00:18:05 +0200

    Complete the logger - it writes in a file, according to a format

--- project/security/utils/Logger.py
+++ project/security/utils/Logger.py
@@ -1,5 +1,6 @@
-from typing import Callable
 from datetime import datetime
+from typing import Callable
+import functools
 import os
 
 # Local imports
@@ -7,29 +8,61 @@ import constants
 
 
 class Logger:
-    def __init__(self):
+    def __init__(self) -> None:
         self.__prepare_logging_environment_hook()
 
-        self.__log_date_format = '%Y-%m-%d-%H-%M-%S'
+        self.__inner_log_date_format = '%Y-%m-%d %H:%M:%S'
+        self.__log_name_date_format = '%Y-%m-%d-%H-%M-%S'
         self.__log_stream_handle = open(
             f'{constants.LOGGING_FOLDER}/'
-            f'{datetime.now().strftime(self.__log_date_format)}.log',
+            f'{datetime.now().strftime(self.__log_name_date_format)}.log',
             'w'
         )
 
     @staticmethod
-    def __prepare_logging_environment_hook():
+    def __prepare_logging_environment_hook() -> None:
         if not os.path.exists(constants.LOGGING_FOLDER):
             try:
                 os.mkdir(constants.LOGGING_FOLDER, 0O740)
             except OSError as e:
                 print(f'ERROR\tUnable to create logging directory. Cause: {e}')
 
-    def log_before(self, func: Callable):
-        self.__log_stream_handle.write(f'Method {func} is executing...')
+    def log_before(self, func: Callable[..., None]) -> ():
+        self.__log_stream_handle.write(
+            f'{datetime.now().strftime(self.__inner_log_date_format)}\tMethod '
+            f'{func.__name__}() is executing...\n'
+        )
 
-    def log_after(self, func: Callable):
-        self.__log_stream_handle.write(f'Method {func} executed...')
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            func()
 
-    def log_exception(self, func: Callable, e: Exception):
-        self.__log_stream_handle.write(f'Method {func} exception thrown: {e}')
+        return wrapper
+
+    def log_after(self, func: Callable[..., None]) -> ():
+        self.__log_stream_handle.write(
+            f'{datetime.now().strftime(self.__inner_log_date_format)}\tMethod '
+            f'{func.__name__}() executed...\n'
+        )
+
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            func()
+
+        return wrapper
+
+    def log_exception(self, func: Callable[..., None]) -> ():
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                func()
+            except Exception as e:
+                self.__log_stream_handle.write(
+                    f'{datetime.now().strftime(self.__inner_log_date_format)}'
+                    f'\tMethod {func.__name__}() exception thrown: '
+                    f'{type(e).__name__}{e.args}\n'
+                )
+            finally:
+                pass
+
+        return wrapper
--- project/security/utils/test.py
+++ project/security/utils/test.py
@@ -3,11 +3,20 @@ from AESCipher import AESCipher
 from PKS7 import PKS7
 from Logger import Logger
 
+G_LOGGER = Logger()
+
+
+@G_LOGGER.log_exception
+@G_LOGGER.log_after
+@G_LOGGER.log_before
+def test():
+    print('Hello')
+    raise ValueError('test')
+
 
 def main():
-    l = Logger()
+    test()
 
 
 if __name__ == "__main__":
     main()
-    print('Hi')
