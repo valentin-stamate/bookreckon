780889ce46c84dedabcb7cbeb925514ee774b12e
Valentin Stamate <stamatevalentin125@gmail.com>
2022-12-05 17:08:22 +0200

    Navbar demo UI

--- project/frontend/src/app/app.module.ts
+++ project/frontend/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { GenreSelectionComponent } from './pages/genre-selection/genre-selection.component';
 import {ReactiveFormsModule} from "@angular/forms";
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     SignupComponent,
-    GenreSelectionComponent
+    GenreSelectionComponent,
+    NavbarComponent
   ],
     imports: [
         BrowserModule,
