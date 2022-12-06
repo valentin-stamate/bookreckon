969feea3dc969fceca57762b1d3a5cabbc821171
reloadedd <reloadedd@protonmail.com>
2022-12-06 23:15:00 +0200

    Create 2 unittests for the SSE scheme

--- project/security/src/main.py
+++ project/security/src/main.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python3
 
+import sys
 from flask import Flask
 from flask_restx import Api, Resource
 
-from Advanced_Software_Engineering_Techniques.BookReckon.project.security.utils import \
-    constants
+# Local imports
+sys.path.append('../utils')
+import constants
 
 app = Flask(constants.APP_NAME)
 api = Api(
     app,
     version=constants.APP_VERSION,
     title=f"{constants.APP_NAME} API",
-    description='REST API for the secure core of Book Recon'
+    description='REST API for the Secure Core of BookRecon'
 )
 ns = api.namespace('encrypt', description='Cryptographic operations')
 
 
 @ns.route('/test')
 @ns.doc(params={})
-class TestApp(Resource):
+class BookRecon(Resource):
     @staticmethod
     def get():
         return '<h1>Hello, World!</h1>'
